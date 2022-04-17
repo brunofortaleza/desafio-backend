4860c740ab1fd09391c688693d53d9e97e18b43f
Bruno Fortaleza <bruno.fortaleza@siedos.com.br>
2022-04-17 19:44:29 -0300

    feat: config bootstrap

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -15,3 +15,6 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// Add this line  
+import '../js/bootstrap_js_files.js'  
